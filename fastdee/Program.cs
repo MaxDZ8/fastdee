99af8803b846d231e10efa5c8a642ae30baa67ad
MaxDZ8 <MassimoDZ8@gmail.com>
2020-12-15 11:34:43 +0100

    Hello world

---
+++ fastdee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fastdee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
