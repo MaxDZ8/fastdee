c78a1053b6867a9ec166b073f44d1c2db4a0b9b9
MaxDZ8 <MassimoDZ8@gmail.com>
2020-12-15 21:18:49 +0100

    Bring in another awesome library!

---
+++ fastdee/Args.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace fastdee
+{
+    class Args
+    {
+        [Value(0, MetaName = "pool", MetaValue = "STRATUM_IP_OR_URI:PORT", Required = true, HelpText = "Pool server endpoint.")]
+        public string Pool { get; set; } = "";
+
+        [Option('f', "fakeVersion", Required = false, HelpText = "Want to mess with the server? Specify your preferred program version to the server!")]
+        public string? PresentationVersion { get; set; }
+    }
+}
--- fastdee/Program.cs
+++ fastdee/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using CommandLine;
+using System.Net.Sockets;
+using System.Net;
 
 namespace fastdee
 {
@@ -6,7 +9,23 @@ namespace fastdee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = new Args();
+            if (Parser.Default.ParseArguments<Args>(args).WithParsed(good => options = good).Tag == ParserResultType.NotParsed)
+            {
+                Environment.ExitCode = 1;
+                return;
+            }
+            // I need to read the parser library documentation and figure out if it can validate those things for me.
+            // I'm inclined to believe the question is not really "if" but rather just "how".
+            var parts = options.Pool.Split(':');
+            var uri = parts[0];
+            var port = ushort.Parse(parts[1]);
+            var addr = Dns.GetHostAddresses(uri)[0];
+            var endpoint = new IPEndPoint(addr, port);
+            using var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(endpoint);
+            Console.Write("made a connection");
+            System.Threading.Thread.Sleep(1234);
         }
     }
 }
