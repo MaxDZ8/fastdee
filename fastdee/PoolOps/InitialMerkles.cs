e97c9fd7c57da7330ffec1dcd9faf8150b25a6f0
MaxDZ8 <MassimoDZ8@gmail.com>
2020-12-19 12:28:58 +0100

    Initial merkles and tests

---
+++ fastdee/PoolOps/InitialMerkles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fastdee.PoolOps
+{
+    /// <summary>
+    /// Contains the various functions used to generate the initial merkle root.
+    /// I know about two for the time being.
+    /// </summary>
+    public static class InitialMerkles
+    {
+        /// <summary>
+        /// Used by most algorithms. It's a simple sha256 of the coinbase. Can't get any simpler than that.
+        /// </summary>
+        static public Mining.MerkleRoot Canonical(byte[] coinbase)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Some coins liked to be different and smart by abusing the original SPHLib to sorta do the hash of an hash.
+        /// </summary>
+        /// <param name="coinbase"></param>
+        /// <returns></returns>
+        static public Mining.MerkleRoot UselesslyComplicated(byte[] coinbase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
