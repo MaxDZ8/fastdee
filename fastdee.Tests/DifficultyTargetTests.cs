33b6109adda806ec83cc4c436c197a90dc62307c
MaxDZ8 <MassimoDZ8@gmail.com>
2020-12-29 21:30:31 +0100

    Difficulty calculation GREEN. I'm not sure I'm supposed to change the tests but real thing is, negative difficulty is really a gimmick. Never really observed ever!

--- fastdee.Tests/DifficultyTargetTests.cs
+++ fastdee.Tests/DifficultyTargetTests.cs
@@ -27,15 +27,11 @@ namespace fastdee.Tests
         /// Made-up calculations to further test equivalency, even for stuff not making sense.
         /// </summary>
         [Theory]
-        [InlineData(12.34, "keccak", 256.0, new ulong[4] { 0x00, 0x00, 0x00, 0x00000014bec7283f })]
-        [InlineData(9999878, "keccak", 128.0, new ulong[4] { 0x00, 0x00, 0xd392a03710000000, 0x000000000001ad7e })]
-        [InlineData(5.78844e+13, "keccak", 64.0, new ulong[4] { 0x00, 0x00, 0x04dcd59db5bbdc80, 0x00 })]
-        [InlineData(1.015654e+37, "keccak", 64.0, new ulong[4] { 0x7c00000000000000, 0x00002180d57b4700, 0x00, 0x00 })]
-        [InlineData(1.1516e-7, "keccak", 64.0, new ulong[4] { 0x00, 0x00, 0x00, 0x847fae2ef954c000 })]
-        [InlineData(-115.245, "keccak", 64.0, new ulong[4] {
-            0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff
-        })]
-        [InlineData(-2.35464156e+5, "keccak", 64.0, new ulong[4] { 0xffffffffffffffff, 0x00, 0x00, 0x00 })]
+        [InlineData(12.34, "keccak", 12.34, new ulong[4] { 0x00, 0x00, 0x1103000000000000, 0x00000014bec7283f })]
+        [InlineData(9999878, "keccak", 9999878, new ulong[4] { 0x00, 0x00, 0xd392a03710000000, 0x000000000001ad7e })]
+        [InlineData(5.78844e+13, "keccak", 5.78844e+13, new ulong[4] { 0x00, 0x00, 0x04dcd59db5bbdc80, 0x00 })]
+        [InlineData(1.015654e+37, "keccak", 1.015654e+37, new ulong[4] { 0x7c00000000000000, 0x00002180d57b4700, 0x00, 0x00 })]
+        [InlineData(1.1516e-7, "keccak", 1.1516e-7, new ulong[4] { 0x00, 0x00, 0x00, 0x847fae2ef954c000 })]
         public void SupportsDifficultyCalculationSynthetic(double diff, string algo, double expectedDiff, ulong[] expectedTarget)
         {
             var mults = Program.ChooseTypicalDifficulties(algo);
@@ -47,12 +43,18 @@ namespace fastdee.Tests
             Assert.Equal(expectedTarget[3], uut.TargD);
         }
 
-        [Fact]
-        public void ZeroIsBadDiff()
+        // I noticed C# diverges from C in those cases (I'm not sure what M8M used to do but it's probably coherent with legacy).
+        // Those cases are super weird anyway.
+        [Theory]
+        [InlineData(.0)]
+        // [InlineData(-.0)] // afaik, -0 is a thing but I guess the compiler crunches it away to 0?
+        [InlineData(-115.245)]
+        [InlineData(-2.35464156e+51)]
+        public void ZeroOrNegativeIsBadDiff(double diff)
         {
             var algo = "keccak";
             var mults = Program.ChooseTypicalDifficulties(algo);
-            Assert.Throws<System.ArgumentException>(() => Program.MakeDiffTarget(algo, mults, .0));
+            Assert.Throws<System.ArgumentException>(() => Program.MakeDiffTarget(algo, mults, diff));
         }
     }
 }
--- fastdee/Program.cs
+++ fastdee/Program.cs
@@ -80,6 +80,67 @@ namespace fastdee
             _ => throw new NotImplementedException()
         };
 
-        internal static DifficultyTarget MakeDiffTarget(string algo, DifficultyMultipliers mults, double serverReq) => throw new NotImplementedException();
+        internal static DifficultyTarget MakeDiffTarget(string algo, DifficultyMultipliers mults, double serverReq) {
+            var shareDiff = mults.Stratum * serverReq;
+            if (shareDiff <= 0.0) throw new ArgumentException(nameof(shareDiff));
+            return algo.ToLowerInvariant() switch
+            {
+                "keccak" => BtcLikeDiffTarget(shareDiff, mults.One),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        /// <summary>
+        /// Difficulty target bits calculation used by most coins.
+        /// </summary>
+        /// <remarks>
+        /// Before you wonder, the donor project here is M8M, AbstractWorkSource.cpp::186.
+        /// MIT license.
+        /// </remarks>
+        static DifficultyTarget BtcLikeDiffTarget(double diff, double diffOneMul)
+        {
+            /*
+            Ok, there's this constant, "truediffone" which is specified as a 256-bit value
+            0x00000000FFFF0000000000000000000000000000000000000000000000000000
+                          |------------------- 52 zeros --------------------|
+            So it's basically aushort(0xFFFF) << (52 * 4)
+            Or: 65535 * ... 2^208?
+            Legacy miners have those values set up, so they can go use double-float division to effectively
+            expand the bit representation and select the bits they care. By using multiple passes, they pull
+            out successive ranges of reductions. They use the following constants:
+            truediffone = 0x00000000FFFF0000000000000000000000000000000000000000000000000000
+            bits192     = 0x0000000000000001000000000000000000000000000000000000000000000000
+            bits128     = 0x0000000000000000000000000000000100000000000000000000000000000000
+            bits64      = 0x0000000000000000000000000000000000000000000000010000000000000000
+            Because all those integers have a reduced range, they can be accurately represented by a double.
+            See diffCalc.html for a large-integer testing framework. */
+            const double BITS_192 = 6277101735386680763835789423207666416102355444464034512896.0;
+            const double BITS_128 = 340282366920938463463374607431768211456.0;
+            const double BITS_64 = 18446744073709551616.0;
+
+            double big = (diffOneMul * TRUE_DIFF_ONE) / diff;
+            Span<double> k = stackalloc double[4] { BITS_192, BITS_128, BITS_64, 1 };
+            Span<ulong> target = stackalloc ulong[4];
+            for (var loop = 0; loop < 4; loop++)
+            {
+                double partial = big / k[loop];
+                ulong magic = (ulong)partial;
+                target[4 - loop - 1] = magic;
+                // ^ note: both legacy and M8M here force this little endian. I don't.
+                // ^ Endianess is something to care about when mix-matching with byte sequences but I plan to stay away this time.
+                partial = magic * k[loop];
+                big -= partial;
+            }
+            return new DifficultyTarget()
+            {
+                ShareDiff = diff,
+                TargA = target[0],
+                TargB = target[1],
+                TargC = target[2],
+                TargD = target[3]
+            };
+        }
+
+        const double TRUE_DIFF_ONE = 26959535291011309493156476344723991336010898738574164086137773096960.0;
     }
 }
