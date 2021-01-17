c8afc9a192f83c4a059c19e71b87ab1dbdbe4091
MaxDZ8 <MassimoDZ8@gmail.com>
2021-01-17 18:36:48 +0100

    UDP server proposal, read-side pump "up".
    After experimentation I have found lwip TCP to be a mess.
    
    UDP will hopefully be more appropriate for embedded devices. This is particularly important for the next generation.

---
+++ fastdee/Devices/Udp/PacketKind.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fastdee.Devices.Udp
+{
+    /// <summary>
+    /// Datagram type, usually serialized as 8 bits.
+    /// </summary>
+    enum PacketKind
+    {
+        /// <summary>
+        /// Broadcasted by devices coming online, reply with server IP address.
+        /// </summary>
+        HelloOnline = 0,
+        /// <summary>
+        /// An idle device requesting for some work. Reserve the requested scan range and send an adjusted header.
+        /// </summary>
+        GimmeWork = 1,
+        /// <summary>
+        /// A working device has found this and is sending back to orchestrator for submitting.
+        /// </summary>
+        FoundNonce = 2
+    }
+}
