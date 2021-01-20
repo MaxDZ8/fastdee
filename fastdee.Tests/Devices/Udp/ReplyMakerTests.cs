408769864a784312f9fe172fc77e176a58e779cc
MaxDZ8 <MassimoDZ8@gmail.com>
2021-01-20 16:42:51 +0100

    Baby steps. Prepare to produce a reply. RED.

---
+++ fastdee.Tests/Devices/Udp/ReplyMakerTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using fastdee.Devices.Udp;
+using System.Net;
+
+namespace fastdee.Tests.Devices.Udp
+{
+    /// <summary>
+    /// Is binary format ok? This time high level to binary blob. Kinda more complicated.
+    /// </summary>
+    public class ReplyMakerTests
+    {
+        [Theory]
+        [InlineData("12.34.255.0", new byte[] { 0, 255, 34, 12 })]         // note the IP value is really irrelevant for it.
+        [InlineData("0.0.0.0", new byte[] { 0, 0, 0, 0 })]                 // and you can feed really bad things
+        [InlineData("255.255.255.255", new byte[] { 255, 255, 255, 255 })] // even those not making sense, no questions asked.
+        public void HelloReplyIsAddress(string serverAddr, byte[] expected)
+        {
+            var ipaddr = IPAddress.Parse(serverAddr);
+            var modelCommon = new byte[] { 0, 1, 2, 3 }; // the arguments are irrelevant as well for the time being.
+            var deviceSpecific = new byte[] { 123, 101, 202 };
+            var magic = new ReplyMaker(ipaddr);
+            var reply = magic.Reply(modelCommon, deviceSpecific);
+            Assert.Same(expected, reply); // the reply is merely for bytes being the ipv4 address.
+        }
+    }
+}
---
+++ fastdee/Devices/Udp/ReplyMaker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fastdee.Devices.Udp
+{
+    /// <summary>
+    /// The <see cref="EventInstantiator"/> mangles the data you received from the socket and, elevates it to real structures.
+    /// The <see cref="ReplyMaker"/> is in some way the opposite, but slightly scaled up.
+    /// It takes care of both building the reply and serializing it to a blob you send over the wire.
+    /// </summary>
+    class ReplyMaker
+    {
+        readonly IPAddress ipaddr;
+
+        public ReplyMaker(IPAddress ipaddr)
+        {
+            this.ipaddr = ipaddr;
+        }
+
+        internal byte[]? Reply(byte[] identificator, byte[] deviceSpecific)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
