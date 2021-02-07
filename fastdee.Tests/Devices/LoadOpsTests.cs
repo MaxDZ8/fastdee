85390ff53083328c3701061b8a7ac9b516c450d0
MaxDZ8 <MassimoDZ8@gmail.com>
2021-02-07 18:55:22 +0100

    I guess it makes sense to have those standalone as well.

---
+++ fastdee.Tests/Devices/LoadOpsTests.cs
@@ -0,0 +1,48 @@
+﻿using fastdee.Devices;
+using Xunit;
+
+namespace fastdee.Tests.Devices
+{
+    public class LoadOpsTests
+    {
+        [Fact]
+        public void CanLoadByte()
+        {
+            byte expect = 5;
+            var blob = new byte[1] { expect };
+            var span = LoadOps.LoadByte(blob, out var loaded);
+            Assert.True(span.IsEmpty);
+            Assert.Equal(expect, loaded);
+        }
+
+        [Fact]
+        public void UshortLoadedFromBigEndian()
+        {
+            ushort expect = 0xABCD;
+            var blob = new byte[2] { 0xCD, 0xAB };
+            var span = LoadOps.LoadUshort(blob, out var loaded);
+            Assert.True(span.IsEmpty);
+            Assert.Equal(expect, loaded);
+        }
+
+        [Fact]
+        public void UintLoadedFromBigEndian()
+        {
+            uint expect = 0xABCDEF01;
+            var blob = new byte[4] { 0x01, 0xEF, 0xCD, 0xAB };
+            var span = LoadOps.LoadUint(blob, out var loaded);
+            Assert.True(span.IsEmpty);
+            Assert.Equal(expect, loaded);
+        }
+
+        [Fact]
+        public void UlongLoadedFromBigEndian()
+        {
+            ulong expect = 0xABCDEF01_23456789;
+            var blob = new byte[8] { 0x89, 0x67, 0x45, 0x23, 0x01, 0xEF, 0xCD, 0xAB };
+            var span = LoadOps.LoadUlong(blob, out var loaded);
+            Assert.True(span.IsEmpty);
+            Assert.Equal(expect, loaded);
+        }
+    }
+}
--- fastdee/Devices/Udp/EventInstantiator.cs
+++ fastdee/Devices/Udp/EventInstantiator.cs
@@ -10,84 +10,36 @@ namespace fastdee.Devices.Udp
     /// </summary>
     class EventInstantiator
     {
-        internal static TurnOnArgs<IPEndPoint> Hello(IPEndPoint origin, Span<byte> buff)
+        internal static TurnOnArgs<IPEndPoint> Hello(IPEndPoint origin, ReadOnlySpan<byte> buff)
         {
-            buff = LoadByte(buff, out var unilen);
-            buff = LoadByte(buff, out var devlen);
-            buff = LoadByteArr(buff, unilen, out var ident);
-            buff = LoadByteArr(buff, devlen, out var devspec);
+            buff = LoadOps.LoadByte(buff, out var unilen);
+            buff = LoadOps.LoadByte(buff, out var devlen);
+            buff = LoadOps.LoadByteArr(buff, unilen, out var ident);
+            buff = LoadOps.LoadByteArr(buff, devlen, out var devspec);
             ThrowIfNotFullyConsumed(buff);
             return new TurnOnArgs<IPEndPoint>(origin, ident, devspec);
         }
 
-        internal static WorkRequestArgs<IPEndPoint> GimmeWork(IPEndPoint origin, Span<byte> buff)
+        internal static WorkRequestArgs<IPEndPoint> GimmeWork(IPEndPoint origin, ReadOnlySpan<byte> buff)
         {
-            buff = LoadUshort(buff, out var kind);
-            buff = LoadUlong(buff, out var reservation);
+            buff = LoadOps.LoadUshort(buff, out var kind);
+            buff = LoadOps.LoadUlong(buff, out var reservation);
             ThrowIfNotFullyConsumed(buff);
             var format = (WireAlgoFormat)kind;
             return new WorkRequestArgs<IPEndPoint>(origin, format, reservation);
         }
 
-        internal static NonceFoundArgs FoundNonce(Span<byte> buff)
+        internal static NonceFoundArgs FoundNonce(ReadOnlySpan<byte> buff)
         {
-            buff = LoadUint(buff, out var wid);
-            buff = LoadUlong(buff, out var offset);
-            buff = LoadByte(buff, out var uintCount);
-            buff = LoadByteArr(buff, uintCount * 4, out var hash);
+            buff = LoadOps.LoadUint(buff, out var wid);
+            buff = LoadOps.LoadUlong(buff, out var offset);
+            buff = LoadOps.LoadByte(buff, out var uintCount);
+            buff = LoadOps.LoadByteArr(buff, uintCount * 4, out var hash);
             ThrowIfNotFullyConsumed(buff);
             return new NonceFoundArgs(wid, offset, hash);
         }
 
-        static Span<byte> LoadByte(Span<byte> buff, out byte value)
-        {
-            value = buff[0];
-            return buff[1..];
-        }
-
-        static Span<byte> LoadByteArr(Span<byte> buff, int elements, out byte[] value)
-        {
-            value = 0 == elements ? Array.Empty<byte>() : new byte[elements];
-            return LoadByteArr(buff, value);
-        }
-
-        static Span<byte> LoadByteArr(Span<byte> buff, byte[] value)
-        {
-            for (var cp = 0; cp < value.Length; cp++) value[cp] = buff[cp];
-            return buff[value.Length..];
-        }
-
-        static Span<byte> LoadUshort(Span<byte> buff, out ushort value)
-        {
-            value = buff[1];
-            value <<= 8;
-            value |= buff[0];
-            return buff[2..];
-        }
-
-        static Span<byte> LoadUint(Span<byte> buff, out uint value)
-        {
-            value = buff[3];
-            value <<= 8;
-            value |= buff[2];
-            value <<= 8;
-            value |= buff[1];
-            value <<= 8;
-            value |= buff[0];
-            return buff[4..];
-        }
-
-        static Span<byte> LoadUlong(Span<byte> buff, out ulong value)
-        {
-            buff = LoadUint(buff, out var lo);
-            buff = LoadUint(buff, out var hi);
-            value = hi;
-            value <<= 32;
-            value |= lo;
-            return buff;
-        }
-
-        static void ThrowIfNotFullyConsumed(Span<byte> buff)
+        static void ThrowIfNotFullyConsumed(ReadOnlySpan<byte> buff)
         {
             if (buff.IsEmpty) return;
             throw new ArgumentException($"{buff.Length} bytes not consumed", nameof(buff));
